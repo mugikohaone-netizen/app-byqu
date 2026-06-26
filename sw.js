e2b92ed49efed640892e8a7e3b54736f9f387a7e
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-06-26 16:10:55 +0900

    🔄 SW更新 vmquldtqk

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mqukwnzj';
+const CACHE = 'app-mquldtqk';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
