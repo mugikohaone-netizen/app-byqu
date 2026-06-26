62f201203c0d2a48e49042d80708ba3a6d6a4244
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-06-26 21:37:28 +0900

    🔄 SW更新 vmqux1rjc

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mquwuxpu';
+const CACHE = 'app-mqux1rjc';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
