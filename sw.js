df11fe7e96a8be3903d7e772462a9b92f75a7d23
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-07-01 06:19:12 +0900

    🔄 SW更新 vmr15g515

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mr0ci055';
+const CACHE = 'app-mr15g515';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
